5b2c076300bd689382d41f5104573f7d8212705d
RicardoPayan <a218206581@unison.mx>
2024-07-26 16:43:15 -0700

    Subiendo repo; ya se puede leer la informacion del un archivo XLSX y desplegar en una tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'examen_radical';
+
+  ExcelData: any
+  constructor(){}
+
+  ReadExcel(event:any){
+    let file = event.target.files[0];
+
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+
+    fileReader.onload = (e)=>{
+      var workBook = XLSX.read(fileReader.result, {type:'binary'});
+      var sheetNames = workBook.SheetNames;
+      this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
+      console.log(this.ExcelData)
+    }
+
+  }
 }
+
+
