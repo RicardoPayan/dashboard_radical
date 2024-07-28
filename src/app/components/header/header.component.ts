bc295245bf875d233cb14b6bc2a2a4627e934bdb
RicardoPayan <a218206581@unison.mx>
2024-07-27 18:40:50 -0700

    Agregando API del clima

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  weather: any;
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.getWeather('Hermosillo').subscribe(
+      (data) => {
+        this.weather = data;
+      },
+      (error) => {
+        console.error('Error fetching weather data', error);
+      }
+    );
+  }
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = '280f253fa1abd40f7f23e07d2ffa6e44'; // Reemplaza con tu API key
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    return this.http.get(url);
+  }
+}
